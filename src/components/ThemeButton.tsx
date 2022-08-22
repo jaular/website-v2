94e2e0532676a63fcef30508f674bd6ae09fed65
José Aular <jose14aular@gmail>
2022-08-22 09:05:01 -0400

    Fix theme color

--- src/components/ThemeButton.tsx
+++ src/components/ThemeButton.tsx
@@ -7,47 +7,47 @@ const ThemeButton = () => {
 
   useEffect(() => setMounted(true), []);
 
-  const svgIcon: JSX.Element =
-    theme === "dark" ? (
-      <svg width="24" height="24" fill="none" aria-hidden="true">
-        <path
-          d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-          fill="currentColor"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        ></path>
-        <path
-          d="M12 4v1M18 6l-1 1M20 12h-1M18 18l-1-1M12 19v1M7 17l-1 1M5 12H4M7 7 6 6"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        ></path>
-      </svg>
-    ) : (
-      <svg width="24" height="24" fill="none" aria-hidden="true">
-        <path
-          d="M18 15.63c-.977.52-1.945.481-3.13.481A6.981 6.981 0 0 1 7.89 9.13c0-1.185-.04-2.153.481-3.13C6.166 7.174 5 9.347 5 12.018A6.981 6.981 0 0 0 11.982 19c2.67 0 4.844-1.166 6.018-3.37ZM16 5c0 2.08-.96 4-3 4 2.04 0 3 .92 3 3 0-2.08.96-3 3-3-2.04 0-3-1.92-3-4Z"
-          fill="currentColor"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        ></path>
-      </svg>
+  if (!mounted)
+    return (
+      <div className="w-6 h-6 rounded-full bg-gray-200/50 dark:bg-black-600 animate-pulse" />
     );
-
-  if (!mounted) return null;
 
   return (
     <button
       className="text-gray-800 dark:text-gray hover:text-black dark:hover:text-white focus:outline outline-offset-4 focus:outline-1 focus:outline-blue"
-      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
     >
       <span className="sr-only">Icon theme</span>
-      {svgIcon}
+      {theme === "light" ? (
+        <svg width="24" height="24" fill="none" aria-hidden="true">
+          <path
+            d="M18 15.63c-.977.52-1.945.481-3.13.481A6.981 6.981 0 0 1 7.89 9.13c0-1.185-.04-2.153.481-3.13C6.166 7.174 5 9.347 5 12.018A6.981 6.981 0 0 0 11.982 19c2.67 0 4.844-1.166 6.018-3.37ZM16 5c0 2.08-.96 4-3 4 2.04 0 3 .92 3 3 0-2.08.96-3 3-3-2.04 0-3-1.92-3-4Z"
+            fill="currentColor"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></path>
+        </svg>
+      ) : (
+        <svg width="24" height="24" fill="none" aria-hidden="true">
+          <path
+            d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+            fill="currentColor"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></path>
+          <path
+            d="M12 4v1M18 6l-1 1M20 12h-1M18 18l-1-1M12 19v1M7 17l-1 1M5 12H4M7 7 6 6"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></path>
+        </svg>
+      )}
     </button>
   );
 };
